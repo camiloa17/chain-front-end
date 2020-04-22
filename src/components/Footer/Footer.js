936048571125663b67a349b08dd963fac37fe8e9
Camilo Dominguez <camiloa17@gmail.com>
2020-04-22 13:01:33 -0700

    fix email

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -12,7 +12,7 @@ const footer =()=>{
                 <h2>Let's talk</h2>
                 <p>Burnaby, BC, Canada</p>
                 <p>+1 604-356-4359</p>
-                <p>contact@chainmarketingdigital.com</p>
+                <p>contact@chaindigital.online</p>
                 <SocialButtons facebook='https://www.facebook.com/chainmarketingdigital' linkedin='https://www.linkedin.com/company/chain-marketing-digital/' whatsapp='https://web.whatsapp.com/send?phone=16043564177&text=I Would love to speak with you!' />
             </div>
             <ContactForm/>
